0f6b7e800b48309d7a4d9d4d9711d15582a60027
Ashutosh Kumar <ashu28jan2003@gmail.com>
2024-10-17 17:22:19 +0530

    Update similarity.js

--- routes/similarity.js
+++ routes/similarity.js
@@ -6,13 +6,13 @@ similarity.post('/getSimilarity',async(req,res)=>{
         const user1 = req.body.user1;
         const user2 = req.body.user2;
     
-        const encodeInterests = (user1, user2) => user1?.map(interest => user2?.includes(interest) ? 1 : 0);
+        const encodeInterests  = (user1, user2) => user1?.map(interest => user2?.includes(interest) ? 1 : 0);
     
-        const user2Interests = encodeInterests(user1, user2);
+        const user2Interests = await encodeInterests(user1, user2);
         
-        const countOnes = user2Interests.filter(num => num === 1).length;
+        const countOnes = await user2Interests.filter(num => num === 1).length;
 
-        const similarity = user1.length > 0 ? countOnes / user1.length : 0;
+        const similarity = await user1.length > 0 ? countOnes / user1.length : 0;
     
         res.status(200).json({ similarity: similarity*100+'%', success: true });
     
